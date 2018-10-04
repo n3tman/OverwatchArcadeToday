b078a9974954d7078f972374414c960864c3059e
Quinten <Quinten@peels.me>
2018-10-04 23:34:58 +0200

    SEO Shit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{env('APP_NAME')}}</title>
+    <title>Overwatch - Today's gamemode (Daily updated)</title>
+
+    <meta charset="UTF-8">
+    <meta name="description" content="Discover today's overwatch arcade gamemode. Daily updated">
+    <meta name="keywords" content="Overwatch, arcade, today, gamemode, gamemodes">
+    <meta name="author" content="bluedog">
+
+    <meta property="og:title" content="Overwatch arcade gamemodes - daily updated">
+    <meta property="og:site_name" content="Overwatch Arcade">
+    <meta property="og:url" content="https://overwatcharcade.today">
+    <meta property="og:description" content="Discover today's Overwatch arcade gamemodes. Site gets updated daily.">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="">
 
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
     <link rel="shortcut icon" href="favicon.ico" />
